e5d77bce4875edc20dc9870d7f725463a83512b0
Bjorn Wanbo <bjornwanbo@gmail.com>
2015-06-11 15:19:49 +0200

    game validations

--- app/models/game.rb
+++ app/models/game.rb
@@ -2,6 +2,10 @@ class Game < ActiveRecord::Base
 
 	has_many :reviews
 
+	validates :name, :presence => true,
+					 :length => {:within => 3..50},
+					 :uniqueness => true
+
 	def to_s
 		"#{name}"
 	end
