2775cec4ece84dda612a158aafd803cb4d47d6e2
Bjorn Wanbo <bjornwanbo@gmail.com>
2015-06-11 14:32:35 +0200

    games CRUD

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,29 +1,51 @@
 class GamesController < ApplicationController
   
   def index
-
+    @games = Game.all
   end
 
   def show
-
+    @game = Game.find(params[:id])
   end
 
   def new
-
+    @game = Game.new
   end
 
   def create
+    @game = Game.new(game_params)
+    if @game.save
+      flash[:notice] = 'Game created.'
+      redirect_to @game
+    else
+      render 'new'
+    end
   end
 
   def edit
+    @game = Game.find(params[:id])
   end
 
   def update
-
+    @game = Game.find(params[:id])
+    if @game.update(game_params)
+      flash[:notice] = 'Game updated.'
+      redirect_to :games
+    else
+      render 'new'
+    end
   end
 
   def destroy
-  
+    Game.find(params[:id]).destroy
+    flash[:notice] = 'Game deleted.'
+    redirect_to :games
   end
-  
+
+  private
+
+  def game_params
+    params.require(:game).permit(:name, :genre, :publisher, :platform)
+  end
+
 end
