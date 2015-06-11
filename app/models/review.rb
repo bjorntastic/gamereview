bba8f4787a3895c79dddab874012fb79c8fcf421
Bjorn Wanbo <bjornwanbo@gmail.com>
2015-06-11 18:30:32 +0200

    add validations for reviews

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,4 +3,10 @@ class Review < ActiveRecord::Base
 	belongs_to :user
 	belongs_to :game
 	
+	validates :rating, :presence => true,
+					   :inclusion => {:within => 0..5}
+
+	validates :title, :presence => true,
+					  :length => {:maximum => 50}
+
 end
